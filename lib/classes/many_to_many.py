b16a72e083f785bb8114d6b8e49bdb695ea18886
Kerry Guarino <kerry.guarino@gmail.com>
2023-11-05 12:49:05 -0500

    begin challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,6 +1,15 @@
 class Coffee:
     def __init__(self, name):
         self.name = name
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) > 2:
+            self._name = value
         
     def orders(self):
         pass
